16b8d22adb9cc60fabcf714ffa25ca7c09959fbd
Paul Larsen <munichpavel@gmail.com>
2018-06-11 22:02:43 +0200

    Add test of smooth fx rate

--- tests/test_expat_fatcat.py
+++ tests/test_expat_fatcat.py
@@ -5,9 +5,13 @@
 
 import pytest
 
+import quandl
+
+from datetime import datetime
+
 from click.testing import CliRunner
 
-from expat_fatcat import expat_fatcat
+from expat_fatcat import expat_fatcat as fatcat
 from expat_fatcat import cli
 
 
@@ -36,3 +40,38 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+@pytest.fixture
+def input_fx_call():
+    
+    return fatcat.quandl_calls['GBP']['FRED']
+    
+    
+@pytest.fixture
+def input_trading_day():
+    return datetime(2018,1,18)
+
+
+@pytest.fixture
+def input_trading_day_before():
+    return datetime(2018, 1, 17)
+
+
+@pytest.fixture
+def input_trading_day_after():
+    return datetime(2018, 1, 19)
+
+
+@pytest.fixture
+def expected_smoothed_fx(input_trading_day_before, input_trading_day_after, input_fx_call):
+    rate_before = quandl.get(input_fx_call, 
+                start_date=input_trading_day_before, end_date=input_trading_day_before)['Value'][0]
+    rate_after = quandl.get(input_fx_call, 
+                start_date=input_trading_day_after, end_date=input_trading_day_after)['Value'][0]
+        
+    return (rate_before + rate_after)/2.0
+    
+
+def test_smooth_fx_rate(input_trading_day, input_fx_call, expected_smoothed_fx):
+    
+    assert fatcat.smooth_fx_rate(input_trading_day, input_fx_call) == expected_smoothed_fx
