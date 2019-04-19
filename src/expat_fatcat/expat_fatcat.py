ce810144ad27ee3a57dd386510f203ba892c09e2
Paul Larsen <munichpavel@gmail.com>
2019-04-19 17:06:52 +0200

    Add abstract fx conversion class

--- src/expat_fatcat/expat_fatcat.py
+++ src/expat_fatcat/expat_fatcat.py
@@ -1,38 +1,63 @@
 """Main module."""
 import os
 from datetime import datetime
+
+from abc import ABC, abstractmethod
+
 import quandl
-
-if 'QUANDL_KEY' in os.environ:
-    quandl.ApiConfig.api_key = os.environ['QUANDL_KEY']
     
     
-
-
-
-def get_rate(date, target_currency, original_currency):
-    '''
-    API call to source of exchange rates, e.g quandl
-    
-    Example usage
-    -------------
-    >>> get_rate(datetime(2019, 4, 19), 'USD', 'EUR')
-    1.12
-    
-    '''
-    
-    return 1.12
-    
-
 class FatcatConverter():
     
-    def __init__(self, currency):
+    def __init__(self, currency, FXRateConverter):
         self.currency = currency
+        self.fx_rate_converter = FXRateConverter(currency)
         
     def _parse_date(self, date_string, date_format='%Y-%m-%d'):
         return datetime.strptime(date_string, date_format)
     
     def _get_rate2usd(self, date_string, date_format='%Y-%m-%d'):
         date = self._parse_date(date_string, date_format)
-        rate = get_rate(date, 'USD', self.currency)
+        rate = self.fx_rate_converter.get_rate(date, 'USD', self.currency)
         return rate
+
+
+    
+
+class RateConverter(ABC):
+    '''Abstract class for currency conversions'''
+    def __init__(self, currency):
+        self.currency = currency
+
+
+    @abstractmethod
+    def get_rate(self, date, target_currency, original_currency):
+        '''Returns exchange rate as a float'''
+        pass
+    
+    
+class QuandlConverter(ABC):
+
+    def __init__(self, currency):
+        
+    #    self.currency = currency
+        self._set_key()
+        self._set_call()
+    
+    def _set_key(self):
+        if 'QUANDL_KEY' in os.environ:
+            quandl.ApiConfig.api_key = os.environ['QUANDL_KEY']    
+            
+    def _set_call(self):
+        if self.currency == 'EUR':
+            self.call_str = 'ECB/EURUSD'
+        elif self.currency == 'GBP':
+            self.call_str = 'FRED/DEXUSUK'
+        else:
+            raise NotImplementedError('Only EUR and GBP to USD rates are implemented')
+            
+    def get_rate(self, date, target_currency, original_currency):
+        '''Returns quandl sourced exchange rate. Note that quandl returns dataframes.'''
+        res = quandl.get(self.call_str, 
+                             start_date=date, end_date=date)
+        return res.iloc[0]['Value']
--- tests/test_expat_fatcat.py
+++ tests/test_expat_fatcat.py
@@ -5,16 +5,35 @@ from datetime import datetime
 
 import expat_fatcat
 
-
-
-    
 @pytest.fixture
-def converter_eur():
-    return expat_fatcat.FatcatConverter('EUR')
+def dummy_rate_converter():
+    class DummyConverter(expat_fatcat.RateConverter):
 
-def test_date_conversion(converter_eur):
-    assert converter_eur._parse_date('2019-04-19') == datetime(2019, 4, 19)
+        def __init__(self, currency):
+            pass
+
+        def _set_call(self):
+            if self.currency == 'FOO':
+                self.call_str = 'FOO/BAR'
+            else:
+                raise NotImplementedError('Only FOO to USD rates are implemented')
+
+
+        def get_rate(self, date, target_currency, original_currency):
+            '''Returns dummy exchange rate'''
+
+            return 1.125
+    return DummyConverter
+
+
+@pytest.fixture
+def converter_foo(dummy_rate_converter):
+    return expat_fatcat.FatcatConverter('FOO', dummy_rate_converter)
+
+def test_date_conversion(converter_foo):
+    assert converter_foo._parse_date('2019-04-18') == datetime(2019, 4, 18)
     
-def test_get_rate(converter_eur):
-    rate = converter_eur._get_rate2usd('2019-04-19')
-    assert rate == 1.12
+
+def test_get_rate(converter_foo):
+    rate = converter_foo._get_rate2usd('2019-04-18')
+    assert rate == 1.125
