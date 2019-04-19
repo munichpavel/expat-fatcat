4e85a4f114c7304caea55790e0386444909d8ca7
Paul Larsen <munichpavel@gmail.com>
2019-04-19 14:57:09 +0200

    Gut and start tdd

--- src/expat_fatcat/__init__.py
+++ src/expat_fatcat/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Paul Larsen"""
 __email__ = 'munichpavel@gmail.com'
 __version__ = '0.1.0'
 
-from .expat_fatcat import quandl_calls, get_fx_rate_smoothed, convert_payments, sum_payments_usd
+from .expat_fatcat import get_rate2usd
--- tests/test_expat_fatcat.py
+++ tests/test_expat_fatcat.py
@@ -2,3 +2,7 @@
 
 import pytest
 import expat_fatcat
+
+def test_get_rate():
+    rate = get_rate2usd('GBP', '2019-04-19')
+    assert rate == 1.12
