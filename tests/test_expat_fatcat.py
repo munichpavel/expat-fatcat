2b454dd3f03c93e80cb259401f4c9d37b8a10391
Paul Larsen <munichpavel@gmail.com>
2019-04-19 14:57:53 +0200

    Fix import

--- tests/test_expat_fatcat.py
+++ tests/test_expat_fatcat.py
@@ -4,5 +4,5 @@ import pytest
 import expat_fatcat
 
 def test_get_rate():
-    rate = get_rate2usd('GBP', '2019-04-19')
+    rate = expat_fatcat.get_rate2usd('GBP', '2019-04-19')
     assert rate == 1.12
