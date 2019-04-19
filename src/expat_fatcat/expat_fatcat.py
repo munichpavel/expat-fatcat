ab5375890e55e96ba2083a935cb2c7fb3d52bb02
Paul Larsen <munichpavel@gmail.com>
2019-04-19 15:49:30 +0200

    Add stub get_rate

--- src/expat_fatcat/expat_fatcat.py
+++ src/expat_fatcat/expat_fatcat.py
@@ -1,6 +1,7 @@
 """Main module."""
 from datetime import datetime
 
+
 class FatcatConverter():
     
     def __init__(self, currency):
@@ -9,5 +10,8 @@ class FatcatConverter():
     def _parse_date(self, date_string, date_format='%Y-%m-%d'):
         return datetime.strptime(date_string, date_format)
     
-    def _get_rate2usd(self, date_string):
-        return 1.12
+    def _get_rate2usd(self, date_string, date_format='%Y-%m-%d'):
+        date = self._parse_date(date_string, date_format)
+        
+        rate = self._get_rate(date, 'USD')
+        return rate
