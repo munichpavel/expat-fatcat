f346adcb38b226010af761d14949e57e310a607a
Paul Larsen <munichpavel@gmail.com>
2019-04-20 18:03:00 +0200

    Rename calculator

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 expat-fatcat helps US taxpayers living abroad file their tax returns by giving you automated conversion of payments in a foreign currency into USD using an exchange rate valid on the day of your payment, as required for form f2555.
 
 See this [notebook](notebooks/fx.ipynb) for the main features of `expat_fatcat` on sample income and housing payment data.
+
+## User story
+
+Alice is an American expat, and to fill out her US taxes each year, she has to convert income payments and relevant expenses (e.g. rent) into US dollars on the day that the payments were made using a valid exchange rate.
--- tests/test_expat_fatcat.py
+++ tests/test_expat_fatcat.py
@@ -12,14 +12,14 @@ def dummy_rate_converter():
 
 
 @pytest.fixture
-def calculator_foo(dummy_rate_converter):
+def dummy_calculator(dummy_rate_converter):
     return expat_fatcat.FatcatCalculator(dummy_rate_converter)
 
 
-def test_date_conversion(calculator_foo):
-    assert calculator_foo._parse_date('2019-04-18') == datetime(2019, 4, 18)
+def test_date_conversion(dummy_calculator):
+    assert dummy_calculator._parse_date('2019-04-18') == datetime(2019, 4, 18)
     
 
-def test_get_rate(calculator_foo):
-    rate = calculator_foo.get_rate('FOO', '2019-04-18')
+def test_get_rate(dummy_calculator):
+    rate = dummy_calculator.get_rate('FOO', '2019-04-18')
     assert rate == 1.125
