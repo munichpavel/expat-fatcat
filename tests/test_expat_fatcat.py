5fe3ae7736c9f611ed9c0b670425da8e50a33fa9
Paul Larsen <munichpavel@gmail.com>
2019-04-21 13:05:17 +0200

    Put basic tests into class

--- tests/test_expat_fatcat.py
+++ tests/test_expat_fatcat.py
@@ -18,21 +18,25 @@ def dummy_calculator(dummy_rate_converter):
     ]
     return expat_fatcat.FatcatCalculator(dummy_rate_converter, 'FOO', payments)
 
+class TestCalculatorBasics:
+    '''
+    Tests for the main functionality of rate calculators.
+    Excludes edge cases such as missing values, invalid arguments, etc.
+    '''
+    def test_date_conversion(self, dummy_calculator):
+        assert dummy_calculator._parse_date('2019-04-18') == datetime(2019, 4, 18)
 
-def test_date_conversion(dummy_calculator):
-    assert dummy_calculator._parse_date('2019-04-18') == datetime(2019, 4, 18)
-    
 
-def test_get_rate(dummy_calculator):
-    rate = dummy_calculator._get_rate('2019-04-18')
-    assert rate == 1.125
-    
+    def test_get_rate(self, dummy_calculator):
+        rate = dummy_calculator._get_rate('2019-04-18')
+        assert rate == 1.125
 
-def test_get_converted_amount(dummy_calculator):
-    amount = 10.
-    assert dummy_calculator._get_converted_amount(amount, '2019-04-18') == 11.25
 
-    
-def test_calculate_agg_payment(dummy_calculator):
-    agg_payment = dummy_calculator()
-    assert agg_payment == 16.875
+    def test_get_converted_amount(self, dummy_calculator):
+        amount = 10.
+        assert dummy_calculator._get_converted_amount(amount, '2019-04-18') == 11.25
+
+
+    def test_calculate_agg_payment(self, dummy_calculator):
+        agg_payment = dummy_calculator()
+        assert agg_payment == 16.875
