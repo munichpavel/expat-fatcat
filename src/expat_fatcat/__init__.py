9e12e0439596f095283f2fbc4d4351934b2341f3
munichpavel <munichpavel@gmail.com>
2020-06-14 14:53:46 +0200

    Bump version: 0.2.1 → 0.2.2

--- src/expat_fatcat/__init__.py
+++ src/expat_fatcat/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Paul Larsen"""
 __email__ = 'munichpavel@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 from .expat_fatcat import FatcatCalculator, QuandlRateConverterToUSD, f2555
