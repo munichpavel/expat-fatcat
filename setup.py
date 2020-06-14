01924bab9f35a0c6bab2a3912601ceae0bd7d304
munichpavel <munichpavel@gmail.com>
2020-06-14 21:18:40 +0200

    Bump version: 0.2.4 → 0.2.5

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requirements = [
 
 setup(
     name='expat_fatcat',
-    version='0.2.4',
+    version='0.2.5',
     description=(
         "expat-fatcat helps US taxpayers living abroad with FX coversions "
         "for their tax returns."
